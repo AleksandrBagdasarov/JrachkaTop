318dfc4b3bfd67950b98b60982fb512c7828b96f
Bahdasarov <sashabagdasarov@gmail.com>
2023-01-04 13:15:53 +0200

    add point model

--- JrachkaTop/api/models/printer.py
+++ JrachkaTop/api/models/printer.py
@@ -1,5 +1,6 @@
-from django.db import models
 from uuid import uuid4
+
+from django.db import models
 
 # Create your models here.
 
@@ -13,7 +14,9 @@ class Printer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
     api_key = models.CharField(max_length=255)
     check_type = models.CharField(max_length=32, choices=types)
-    point_id = models.CharField(max_length=32)
+    point_id = models.ForeignKey("point", on_delete=models.CASCADE)
 
     class Meta:
+        db_table = "printer"
+        verbose_name = "printer"
         verbose_name_plural = "printer"
