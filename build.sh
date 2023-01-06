1c53ae040738b71659386d7f8adc2601a5b5d9b9
Bahdasarov <sashabagdasarov@gmail.com>
2023-01-06 16:29:11 +0200

    update readme.md

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ docker-compose up -d --build wkhtmltopdf
 docker-compose up -d --build redis
 docker-compose up -d --build db
 
-cd JrachkaTop && python -m celery -A JrachkaTop worker
+cd JrachkaTop && python3 manage.py migrate && python3 manage.py loaddata data
+#gunicorn JrachkaTop.wsgi &
+python -m celery -A JrachkaTop worker
